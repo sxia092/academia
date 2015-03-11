050861d21299446a7ce9ab7b165192e8f1559e20
Illya Starikov <illya@starikov.co>
2015-03-11 14:07:00 -0500

    Initial Commit

---
+++ src/cs1580-intro-to-programming-lab/lab8/lab8/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab8
+//
+//  Created by Illya Starikov on 3/11/15.
+//  Copyright (c) 2015 Illya Starikov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
