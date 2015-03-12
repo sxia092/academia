11d46af42bf1fd8aa19b34871929b4c4bf182127
Illya Starikov <illya@starikov.co>
2015-03-12 15:35:55 -0500

    Initial Commit

---
+++ src/cs1570-intro-to-programming/Homework7/Homework7/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Homework7
+//
+//  Created by Illya Starikov on 3/12/15.
+//  Copyright (c) 2015 Illya Starikov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
