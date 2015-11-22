ee11537ebea6c6bdefb15ee89d60f310aae5fae3
REDACTED <REDACTED@example.com>
2015-11-21 20:05:58 -0600

    Initial Commit

---
+++ src/cs1510-data-structures/Homework #7/Homework #7/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Homework #7
+//
+//  Created by Illya Starikov on 11/21/15.
+//  Copyright © 2015 Illya Starikov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
