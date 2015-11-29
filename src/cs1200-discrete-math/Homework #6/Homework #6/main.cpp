5bde2a8f404503449bf261d5ff3e5ef87689f5a4
REDACTED <REDACTED@example.com>
2015-11-29 12:36:57 -0600

    Initial Commit

---
+++ src/cs1200-discrete-math/Homework #6/Homework #6/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Homework #6
+//
+//  Created by Illya Starikov on 11/29/15.
+//  Copyright © 2015 Illya Starikov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
