e0b9ba8c2a054c6129ca29e08bad69bd831731d7
REDACTED <REDACTED@example.com>
2016-01-31 16:19:41 -0600

    Initial Commit

---
+++ src/cs2500-algorithms/Homework #1/Homework #1/Homework #1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Homework #1
+//
+//  Created by Illya Starikov on 1/31/16.
+//  Copyright © 2016 Illya Starikov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
