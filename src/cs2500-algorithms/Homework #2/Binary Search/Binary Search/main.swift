8da9e0dfff9a9406d1496c2558f4bb350a5e3ccf
Illya Starikov <illya@starikov.co>
2016-02-07 14:58:50 -0600

    Initial Commit

---
+++ src/cs2500-algorithms/Homework #2/Binary Search/Binary Search/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Binary Search
+//
+//  Created by Illya Starikov on 2/7/16.
+//  Copyright © 2016 Illya Starikov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
