42faf9f8c5f322231c41199187d3cfe33a826d8d
Illya Starikov <illya@starikov.co>
2016-03-15 17:23:32 -0500

    Finished dynamic programming, almost finished.

--- src/cs2500-algorithms/CS2500-Project1/Source Code/Project Code.py
+++ src/cs2500-algorithms/CS2500-Project1/Source Code/Project Code.py
@@ -26,31 +26,31 @@ class App:
 
 def bruteForce(smartphone, memoryGoal):
     # Make the minimum the sum of all, just for an initial value
-    
+
     minimum = 0
     greatestSubset = 0
 
     for App in smartphone:
         minimum += App.cost
-        
+
     # Iterate over the 2^n subsets
     for i in range(0, int(pow(2,len(smartphone)))):
         binaryRepresenation = bin(i) # A binary representation of the binary
         subset = binaryRepresenation.replace("0b", "") #bin returns 0b..., so this returns ..
-        
+
         totalMemory = 0
         totalCost = 0
-        
+
         for j in range(len(subset)):
             if subset[j] == '1':
                 totalMemory += smartphone[j].memory #* ord(subset[i])
                 totalCost += smartphone[j].cost #* ord(subset[i])
-    
+
         if totalMemory >= memoryGoal and totalCost < minimum:
             minimum = totalCost
             greatestSubset = i
-          
-        
+
+
     binaryRepresenation =  bin(greatestSubset)
     subset = binaryRepresenation.replace("0b", "")
 
@@ -58,14 +58,82 @@ def bruteForce(smartphone, memoryGoal):
     totalMemory = 0
 
     optimalSolution = []
-    
+
     for i in range(len(subset)):
         if subset[i] == '1':
             optimalSolution.append(smartphone[i].number)
             totalCost += smartphone[i].cost
             totalMemory += smartphone[i].memory
-    
+
     return (optimalSolution, totalCost, totalMemory)
+#numofApps = number of applications open
+#MemoryGoal = amount of memeory needed to free up
+#maxMem = maximum memory of the device
+def DelTable(Apps, MemoryGoal):
+    numofApps = len(Apps)
+    maxMem = 0
+    for i in range(0,numofApps):
+        maxMem += Apps[i].memory
+
+    DelTable = [[0 for j in range(5)] for i in range(maxMem)]
+    #label rows
+    for j in range(0, maxMem):
+        DelTable[j][0] = j
+
+    for j in range(0, maxMem):
+        DelTable[j][3] = []
+        
+    DelTable[0][0] = 0
+    DelTable[0][1] = 0
+    DelTable[0][2] = 0
+    DelTable[0][4] = 0
+
+    #base case
+    #Asssume the first app has the smallest memory and the lowest cost 
+    BaseCase = Apps[0].number
+    BaseMem = Apps[0].memory
+    BaseCost = Apps[0].cost
+    #Check against every other app in the list 
+    for i in range (0,numofApps):
+        if(Apps[i].memory <= BaseMem):
+            if(Apps[i].cost < BaseCost):
+                BaseCost = round(Apps[i].cost)
+                BaseMem = round(Apps[i].memory)
+                BaseCase = round(Apps[i].number)
+
+    #FIll up to the base case line
+    illyaisawesome = int(round(BaseCase))
+    for i in range(1, illyaisawesome):
+        DelTable[i][1] = Apps[illyaisawesome].memory
+        DelTable[i][2] = BaseCost # ILLYA CHANGED THIS
+        DelTable[i][3].append(Apps[i].number)
+        
+    #Fill the rest of the table
+    for j in range (illyaisawesome, maxMem):
+        for i in range(0, numofApps):
+            if(Apps[i].memory == j and Apps[i].cost < DelTable[j-1][2] + DelTable[illyaisawesome][2]):
+                DelTable[j][1] = Apps[i].memory
+                DelTable[j][2] = Apps[i].cost
+                DelTable[j][3].append(Apps[i]) # ILLYA CHANGED THIS
+                DelTable[j][4] = 0
+            else:
+                DelTable[j][1] = DelTable[j-1][1]+1
+                PlaceHolder = j - BaseCost
+                claireisawesome = int(round(PlaceHolder))
+                Sum1 = DelTable[claireisawesome][2]
+                Sum2 = DelTable[illyaisawesome][2]
+                DelTable[j][2] = Sum1 + Sum2
+                DelTable[j][3].append(Apps[i]) # ILLYA CHANGED THIS
+                
+    Goals = int(round(MemoryGoal))
+    
+    sumOf = 0 
+    for app in DelTable[Goals][3]:
+        sumOf += app.cost
+        
+#    DelTable[Goals][2]    
+    Answer = (DelTable[Goals][3], sumOf, DelTable[Goals][1]) 
+    return Answer
 
 def GreedKnap(Phone, MemGoal):
     Solution = []
@@ -133,7 +201,7 @@ while (True):
         #Outputting array for testing purposes
         for j in Smartphone:
             print j, " "
-
+        
         if (inputsize < 50):
             for i in range(10):
                 t0 = time.clock()
@@ -150,5 +218,3 @@ while (True):
             Time.append(elapsed)
         averagetime = sum(Time)/len(Time)
         PrintOut(3, inputsize, data, averagetime, MemGoal)
-#    sys.exit("Finished") Illya's Debugging
-    
