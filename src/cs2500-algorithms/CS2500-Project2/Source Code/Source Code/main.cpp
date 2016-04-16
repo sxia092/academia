9b178f98b3b6545e2469a3ef6eb71e9baa22c533
Illya Starikov <illya@starikov.co>
2016-04-16 15:15:44 -0500

    Added source code directory and updated LaTeX.

---
+++ src/cs2500-algorithms/CS2500-Project2/Source Code/Source Code/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Source Code
+//
+//  Created by Illya Starikov on 4/16/16.
+//  Copyright © 2016 Illya Starikov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
