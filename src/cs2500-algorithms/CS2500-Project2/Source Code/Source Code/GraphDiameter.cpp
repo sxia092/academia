233dd541355bbf50e1390a8e01706ae7f0158120
Claire Trebing <Claire Trebing>
2016-05-01 12:30:08 -0500

    Finished betweeness, halfway done with GphDiameter

---
+++ src/cs2500-algorithms/CS2500-Project2/Source Code/Source Code/GraphDiameter.cpp
@@ -0,0 +1,9 @@
+//
+//  GraphDiameter.cpp
+//  Source Code
+//
+//  Created by Claire Trebing on 5/1/2016
+//  
+
+#include "Functions.hpp"
+#include "GraphDiameter.hpp"
---
+++ src/cs2500-algorithms/CS2500-Project2/Source Code/Source Code/GraphDiameter.hpp
@@ -0,0 +1,19 @@
+//
+//  GraphDiameter.hpp
+//  Source Code
+//
+//  Created by Claire Trebing on 5/1/16.
+//
+
+#ifndef GraphDiameter_hpp
+#define GraphDiameter_hpp
+
+#include <stdio.h>
+#include <iostream> 
+
+#include "Functions.hpp"
+#include "Constants.h"
+
+
+
+#endif /* GraphDiameter_hpp */
