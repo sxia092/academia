798d9840dbfc54bf5be0d95bd5ef928fe939f446
Illya Starikov <illya@starikov.co>
2016-05-01 22:26:20 -0500

    updated report

--- src/cs2500-algorithms/CS2500-Project2/Source Code/Source Code/Betweeness.cpp
+++ src/cs2500-algorithms/CS2500-Project2/Source Code/Source Code/Betweeness.cpp
@@ -7,7 +7,7 @@
 //
 #include "Betweeness.hpp"
 
-void VertexPathReconstruction(const AdjacencyMap & map, int (&betweeness)[MAX_VERTEX], bool Directness, bool Weightness){
+void VertexPathReconstruction(const AdjacencyMap & map, int betweeness[], bool Directness, bool Weightness){
 	// Floyd - Walsh algorithm
     const int size = static_cast<int>(map.size()) + 2; // One for the index of map, other for index of solution vector
     auto solution = std::vector<std::vector<int>>(size, std::vector<int>(size, INFINITY)); // 2D Adjacency Matrix with size of map and default values of infinity
