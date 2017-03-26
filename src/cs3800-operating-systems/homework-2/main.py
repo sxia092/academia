3ec5a02b05fa49932dcb016aee7e6dc883e92e99
Illya Starikov <illya@starikov.co>
2017-03-26 12:52:09 -0500

    Added homework-2 directory with main py file

---
+++ src/cs3800-operating-systems/homework-2/main.py
@@ -0,0 +1,13 @@
+#
+#  main.py
+#  CS3800 — Homework #2
+#
+#  Created by Illya Starikov on 26/03/2017
+#  Copyright 2017. Illya Starikov. All rights reserved.
+#
+
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
