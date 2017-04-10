63437f6fddb169beaaf7fe370fe9549c1b5a5d6e
Illya Starikov <illya@starikov.co>
2017-04-10 10:27:19 -0500

    Added basic plotting

---
+++ src/math3304-differential-equations/plotter/plot.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def step(t_0, t):
+    results = []
+    for unit in t:
+        results += [1 if unit > t_0 else 0]
+
+    return results
+
+
+chapterNumber = 6
+sectionNumber = 5
+problemNumber = 1
+
+bounds = [-200, 200]  # y-axis bounds
+t = np.arange(-30.0, 30.0, 0.01)  # x-axis bounds
+s = np.exp(-t) * np.cos(t) - 3 * np.exp(-t) * np.sin(t) + step(np.pi, t) * np.exp(-t + np.pi) * np.sin(t - np.pi)  # equation of t
+
+plt.plot(t, s)
+
+plt.xlabel('t')
+plt.ylabel('y(t)')
+plt.title("Section {0}.{1}, Problem #{2}".format(chapterNumber, sectionNumber, problemNumber))
+
+plt.grid(True)
+plt.ylim(bounds)
+
+plt.savefig("{0}.{1}-problem-{2}.png".format(chapterNumber, sectionNumber, problemNumber))
+plt.show()
