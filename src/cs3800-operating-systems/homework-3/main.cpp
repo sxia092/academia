a3c8a5ff7fa3b91155facb6e094440b0f18e2470
Illya Starikov <illya@starikov.co>
2017-04-17 21:19:05 -0500

    Added bindings to server

--- src/cs3800-operating-systems/homework-3/main.cpp
+++ src/cs3800-operating-systems/homework-3/main.cpp
@@ -15,10 +15,18 @@
 #include <stdlib.h>
 #include <errno.h>
 
-
+const auto PORT = 9000;
+const auto MAX_HOSTNAME = 512;
 
 int main(int argc, char *argv[]) {
-    int socketHandle;
+    struct sockaddr_in serverSocketInfo;
+    struct hostent* hostNamePointer;
+
+    int socketHandle, socketConnection;
+    char systemHost[MAX_HOSTNAME + 1]; // For C-APIS
+
+
+    bzero(&serverSocketInfo, sizeof(sockaddr_in));  // Clear structure memory
 
     // Use Internet IPV4 family (AF_INET), SOCKSTRAM since we're doing TCP, and IP
     // See man page for this
@@ -26,4 +34,30 @@ int main(int argc, char *argv[]) {
         close(socketHandle);
         exit(EXIT_FAILURE);
     }
+
+    gethostname(systemHost, MAX_HOSTNAME);  // Get the name of this computer we are running on
+    if((hostNamePointer = gethostbyname(systemHost)) == NULL) {
+        std::cerr << "System Hostname Misconfigured\n";
+        exit(EXIT_FAILURE);
+    }
+
+    // Load system information into socket data structures
+    serverSocketInfo.sin_family = AF_INET;
+
+    // Use any address available to the system.
+    // A socket client will specify the server address to connect to.
+    serverSocketInfo.sin_addr.s_addr = htonl(INADDR_ANY); // Translate long integer to network byte order.
+    serverSocketInfo.sin_port = htons(PORT);        // Set port number
+
+    // Bind the socket to a local socket address, act as a server
+    if(bind(socketHandle, (struct sockaddr *) &serverSocketInfo, sizeof(struct sockaddr_in)) < 0) {
+        close(socketHandle);
+        std::cerr << "Couldn't bind to local address.\n";
+        exit(EXIT_FAILURE);
+    }
+
+    listen(socketHandle, 10); // Set maximum length of the queue of pending connections for the listening socket
+    // 10 is pretty much arbitrary.
+
+
 }
