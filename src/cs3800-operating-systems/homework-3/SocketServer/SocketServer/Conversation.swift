84d5855bfdee51ef85f71075688dab9709a13994
Illya Starikov <illya@starikov.co>
2017-04-20 11:45:41 -0500

    Added a port of JSQMessageViewController
    My code is bad.. Really bad.

---
+++ src/cs3800-operating-systems/homework-3/SocketServer/SocketServer/Conversation.swift
@@ -0,0 +1,15 @@
+//
+//  Conversation.swift
+//  SwiftExample
+//
+//  Created by Dan Leonard on 5/11/16.
+//  Copyright © 2016 MacMeDan. All rights reserved.
+//
+
+import Foundation
+
+struct Conversation {
+    let userName: String?
+    let id: String?
+    let latestMessage: String?
+}
