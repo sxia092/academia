58240ff69b61305cdb3311d4b2b140529a1e7d4a
Illya Starikov <illya@starikov.co>
2017-09-18 13:20:40 -0500

    Merge branch 'master' of https://git-classes.mst.edu/ane3md/splatoonio

---
+++ src/cs4096-software-systems-development/splatoonio/android/app/src/main/java/com/example/splatoonio/splatoonio/Menu.kt
@@ -0,0 +1,12 @@
+package com.example.splatoonio.splatoonio
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
