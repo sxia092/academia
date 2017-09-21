50a6154d1c3c71005dcb7d21b4086b02cc68ecea
Illya Starikov <illya@starikov.co>
2017-09-20 19:48:50 -0500

    Add utilities file for all misc functions
    Also change board dimensions to be max of height, width

---
+++ source/utilities.py
@@ -0,0 +1,131 @@
+#!/usr/local/bin/python3
+#
+# utilities.py
+# source
+#
+# Created by Illya Starikov on 09/20/17.
+# Copyright 2017. Illya Starikov. All rights reserved.
+#
+
+import random
+
+import json
+import time
+import os
+import sys
+
+from shape import Shape
+
+
+def random_arbitrary(floor, ceiling):
+    return random.randint(floor, ceiling)
+
+
+# x_bounds is a tuple (min, max) the values x can take
+# same for y
+def generate_random_point(row_bounds, column_bounds):
+    row_min, row_max = row_bounds
+    column_min, column_max = column_bounds
+    return (random_arbitrary(row_min, row_max), random_arbitrary(column_min, column_max))
+
+
+def get_file_contents(filename):
+    with open(filename) as file_:
+        return file_.read().split("\n")
+
+
+def get_config_contents(config_filename):
+    contents = get_file_contents(config_filename)
+    contents = "\n".join(contents)
+
+    json_data = json.loads(contents)
+    return json_data
+
+
+def static_vars(**kwargs):
+    def decorate(func):
+        for k in kwargs:
+            setattr(func, k, kwargs[k])
+        return func
+    return decorate
+
+
+def configure():
+    configuration = get_config_contents(sys.argv[1])
+    max_iterations = configuration["iterations"]
+    number_of_runs = configuration["runs"]
+
+    if configuration["predefined-seed"]:
+        seed_ = configuration["seed"]
+        random.seed(seed_)
+    else:
+        seed_ = random_arbitrary(0, 2**31 - 1)
+        random.seed(seed_)
+
+    filename = sys.argv[2]
+    path_to_solution = configuration["path-to-solution"]
+    path_to_log = configuration["path-to-log"]
+
+    if not os.path.isdir(path_to_solution):
+        os.makedirs(path_to_solution)
+
+    if not os.path.isdir(path_to_log):
+        os.makedirs(path_to_log)
+
+    return filename, max_iterations, number_of_runs, seed_, path_to_log, path_to_solution
+
+
+def get_shapes_and_board_dimensions(filename):
+    file_contents = get_file_contents(filename)
+
+    shapes = []
+    max_width = 0
+    max_height, _ = [int(x) for x in file_contents[0].split(' ')]
+
+    for line in file_contents[1:]:
+        if line != "":
+            input_ = line.split(' ')
+            shape = Shape(input_)
+
+            shapes += [shape]
+            max_width += max(shape.get_width(), shape.get_height())
+
+    return shapes, (max_width, max_height - 1)  # -1 because the height given isn't zero indexed
+
+
+def output_to_file(filename, string):
+    with open(filename, 'a') as out:
+        out.write(string + "\n")
+
+
+def output_solution(solution_directory, solution):
+    # current_date_time = time.strftime("%Y-%m-%d_%H-%M-%S")
+    # filename = solution_directory + "/solution-" + current_date_time + ".txt"
+    # run = solution.get_run()
+
+    filename = solution_directory + "solution_this_one.txt"
+    # output_to_file(filename, "-- Solution Found on Run #{} --".format(run))
+    for shape in solution.get_shapes():
+        row, column = shape.get_start_position()
+        rotations = shape.get_rotations()
+        output_to_file(filename, "{},{},{}".format(column, row, rotations))
+
+
+@static_vars(already_output_header=False, filename=None)
+def output_log(path_to_log, configuration_parameters, fitnesses, run):
+    if output_log.filename is None:
+        current_date = time.strftime("%Y-%m-%d")
+        output_log.filename = path_to_log + "/log-" + current_date + ".txt"
+
+    if not output_log.already_output_header:
+        output_log.already_output_header = True
+
+        for key, value in configuration_parameters.items():
+            output_to_file(output_log.filename, "{}: {}".format(key.ljust(20), str(value)))
+
+    output_to_file(output_log.filename, "-- Run #{} --".format(run))
+
+    for iteration, fitness in fitnesses:
+        output_to_file(output_log.filename, "{}\t{}".format(iteration, fitness))
+
+    output_log.last_run = run
