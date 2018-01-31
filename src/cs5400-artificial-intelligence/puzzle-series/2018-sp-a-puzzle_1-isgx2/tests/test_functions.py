c53da7ff2f2f97965883f022404e6b31867e5b1b
Illya Starikov <illya@starikov.co>
2018-01-30 21:36:31 -0600

    Merge branch 'master' of https://git-classes.mst.edu/2018-SP-CS5400/2018-sp-a-puzzle_1-isgx2

---
+++ src/cs5400-artificial-intelligence/puzzle-series/2018-sp-a-puzzle_1-isgx2/tests/test_functions.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3
+#
+# test_functions.py
+# tests
+#
+# Created by Illya Starikov on 01/28/18.
+# Copyright 2018. Illya Starikov. All rights reserved.
+#
+
+import unittest
+import sys
+
+sys.path.append('./src')
