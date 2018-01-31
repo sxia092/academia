94645337d17e1cfa2fdcd4289813d5ffea8a473d
Illya Starikov <illya@starikov.co>
2018-01-30 19:37:17 -0600

    Add Running AI
    With timer!

--- src/cs5400-artificial-intelligence/puzzle-series/2018-sp-a-puzzle_1-isgx2/src/functions.py
+++ src/cs5400-artificial-intelligence/puzzle-series/2018-sp-a-puzzle_1-isgx2/src/functions.py
@@ -111,3 +111,100 @@ def generate_game_from_namespace(namespace):
         pool=namespace.pool,
         grid=namespace.grid
     )
+
+
+def calculate_new_row_column(row_column_pair, direction):
+    """From a row_column_pair a direction, calculate what the complementary
+    point would be
+
+    Args:
+        row_column_pair (int, int): A tuple of value (row, column) of the initial point.
+        direction (Direction): A Direction to determine which way the point should be swapped.
+
+    Returns:
+        (int, int): The point after applying the direction onto the new point.
+    """
+
+    as_unit_vector = direction.unit_vector
+    return (row_column_pair[0] + as_unit_vector[0], row_column_pair[1] + as_unit_vector[1])
+
+
+def determine_swaps(node, game):
+    """Determine the swaps that got the game into the new state.
+
+    Args:
+        node (SearchNode): The SearchNode of the solution.
+        game (MechanicalMatch): The initial game setup (used to get the pool height).
+
+    Returns:
+        list of ((int, int), (int, int)): The swaps of the form (old_x, old_y), (new_x, new_y) that led to the solution.
+    """
+    runner = node
+    swaps = []
+
+    while runner.parent:
+        before = runner.action.row_column_pair
+        after = calculate_new_row_column(runner.action.row_column_pair, runner.action.direction)
+
+        # To get into the weird format Dr. T wants
+        before = (before[1], before[0] + game.pool_height)
+        after = (after[1], after[0] + game.pool_height)
+
+        swaps += [(before, after)]
+        runner = runner.parent
+
+    return reversed(swaps)
+
+
+def generate_swaps_string(swaps):
+    """String-ify the weird swap data structure
+
+    Args:
+        swaps (list of ((int, int), (int, int))): The swaps of the form (old_x, old_y), (new_x, new_y)
+        that led to the solution.
+
+    Returns:
+        str: A nice, clean string of the swaps (with newlines).
+    """
+
+    string = ""
+
+    for before, after in swaps:
+        string += "{}, {}\n".format(before, after)
+
+    return string[:-1]
+
+
+def output_to_file(filename, content):
+    """Output content to a file.
+
+    Args:
+        filename (str): The output filename.
+        content (str): What is to go into the actual file
+    """
+
+    with open(filename, "w") as text_file:
+        text_file.write(content)
+
+
+def output_solution(filename_of_output, filename_of_input, timer_difference, solution_node, game):
+    """Outputs the solution to stdout and, optionally, to a file
+
+    Args:
+        filename_of_output (str): The filename to output the solution to, or None if output to file
+        is not desired.
+        filename_of_input (str): The original file the game parameters were taken from.
+        timer_different (float): The amount of time (in seconds) the algorithm took to run
+        solution_node (SearchNode): The SearchNode where the solution was found
+        game (MechanicalMatch): The initial game instance.
+    """
+
+    file_contents = get_file_contents(filename_of_input)
+    swap_string = generate_swaps_string(determine_swaps(solution_node, game))
+
+    print(file_contents)
+    print(swap_string)
+    print(timer_difference)
+
+    if filename_of_output:
+        output_to_file(filename_of_output, "{}\n{}\n{}\n".format(file_contents, swap_string, timer_difference))
--- src/cs5400-artificial-intelligence/puzzle-series/2018-sp-a-puzzle_1-isgx2/src/main.py
+++ src/cs5400-artificial-intelligence/puzzle-series/2018-sp-a-puzzle_1-isgx2/src/main.py
@@ -7,14 +7,25 @@
 # Copyright 2018. Illya Starikov. All rights reserved.
 #
 
-from functions import parse_arguments, parse_game_parameters, get_file_contents, generate_game_from_namespace
+from functions import parse_arguments, parse_game_parameters, get_file_contents, generate_game_from_namespace, output_solution
+from timer import Timer
+from AI import AI
 
 
 def main():
+    timer = Timer()
+
     filename, output_path = parse_arguments()
     game_parameters = parse_game_parameters(get_file_contents(filename))
 
     game = generate_game_from_namespace(game_parameters)
+    ai = AI(game)
+
+    timer.start()
+    solution = ai.breadth_first_tree_search()
+    timer.stop()
+
+    output_solution(output_path, filename, timer.difference, solution, game)
 
 
 if __name__ == "__main__":
