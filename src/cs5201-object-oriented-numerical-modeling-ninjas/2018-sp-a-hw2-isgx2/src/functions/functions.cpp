042563532725e9af84a0e628b80ad485f711f3e3
Illya Starikov <illya@starikov.co>
2018-02-12 21:23:20 -0600

    Add better file parsing

--- src/cs5201-object-oriented-numerical-modeling-ninjas/2018-sp-a-hw2-isgx2/src/functions/functions.cpp
+++ src/cs5201-object-oriented-numerical-modeling-ninjas/2018-sp-a-hw2-isgx2/src/functions/functions.cpp
@@ -8,65 +8,29 @@
 
 #include "functions.h"
 
-bool enoughParametersPassed(const int argc) {
-    return argc > 1;
+void ensureEnoughArguments(const int argc) {
+    if (argc < 2) {
+        displayErrorAndQuit("Not enough parameters. Terminating.");
+    }
 }
 
-void displayErrorAndQuit(const std::string output) {
-    std::cout << output << std::endl;
+void displayErrorAndQuit(const std::string& output) {
+    std::cerr << output << std::endl;
     exit(-1);
 }
 
-std::vector<std::pair<double, double>> parseFileContents(const std::string filename) {
-    if (!fileExists(filename)) {
-        // I miss String.Format :(
-        displayErrorAndQuit("File " + filename + " does not exists. Terminating.");
-    }
-
-    auto file = readFile(filename);
-    auto fileSplitOnNewline = split(file, '\n');
-    auto polarPairs = std::vector<std::pair<double, double>>();
-
-    if (fileSplitOnNewline.size() == 0) {
-        displayErrorAndQuit("File " + filename + " is empty. Nice try, Nathan. Terminating.");
-    } else {
-        try {
-            auto numberOfParameter = stringToInt(fileSplitOnNewline[0]);
-
-            // Static cast to get ride of unsigned int/int comparison warning
-            if (numberOfParameter < static_cast<int>(fileSplitOnNewline.size()) - 1) {
-                displayErrorAndQuit("Not enough polar pairs in input file. Terminating.");
-            } else if (numberOfParameter < 0) {
-                displayErrorAndQuit("Number of polar pairs cannot be negative. Terminating.");
-            }
-
-            for (int i = 1; i < numberOfParameter; i++) {
-                auto inputSplit = split(fileSplitOnNewline[i], ' ');
-
-                if (inputSplit.size() < 2) throw std::logic_error("Freaking too little input");
-
-                auto pair = std::make_pair(stringToDouble(inputSplit[0]), stringToDouble(inputSplit[1]));
-                polarPairs.push_back(pair);
-            }
-        } catch(const std::logic_error error) {
-            displayErrorAndQuit("Data is not in proper format. Terminating.");
-        }
-    }
-
-    return polarPairs;
-}
-
-int stringToInt(const std::string someString) {
+int stringToInt(const std::string& someString) {
     return std::stoi(someString); // stoi is just not descriptive enough for me.
 }
 
-double stringToDouble(const std::string someString) {
+double stringToDouble(const std::string& someString) {
     // stod  is just not descriptive enough for me.
     // and it sounds like an noun...
     return std::stod(someString);
 }
 
-std::vector<std::string> split(const std::string someString, const char delimiter) {
+
+std::vector<std::string> split(const std::string& someString, const char delimiter) {
     auto stream = std::stringstream(someString);
     std::string segment;
     std::vector<std::string> splitText;
@@ -79,12 +43,12 @@ std::vector<std::string> split(const std::string someString, const char delimite
 }
 
 
-bool fileExists(const std::string filename) {
+bool fileExists(const std::string& filename) {
     // is_regular_file because exists checks for files and directories
     return filesystem::exists(filename) && filesystem::is_regular_file(filename);
 }
 
-std::string readFile(filesystem::path path) {
+std::string readFile(const filesystem::path& path) {
     const auto size = filesystem::file_size(path);
 
     std::ifstream fin{ path };
@@ -96,3 +60,28 @@ std::string readFile(filesystem::path path) {
     return contents;
 }
 
+std::string readInputFile(const std::string& filename) {
+    if (!fileExists(filename)) {
+        // I miss String.Format :(
+        displayErrorAndQuit("File " + filename + " does not exists. Terminating.");
+    }
+
+    auto fileContents = readFile(filename);
+    auto stringStream = std::ostringstream();
+
+    auto FLOAT        = R"([-+]?[0-9]*\.?[0-9]*)";
+    auto POSITIVE_INT = R"([1-9]\d*)";
+    auto WHITESPACE   = R"(\s*)";
+
+    stringStream << WHITESPACE << POSITIVE_INT << WHITESPACE << R"(\n()" << WHITESPACE << FLOAT << R"(\s+)" << FLOAT << WHITESPACE << R"(\n)*)";
+
+    auto regexString = stringStream.str();
+    auto regex       = std::regex(regexString);
+    auto match       = std::smatch();
+
+    if (!std::regex_search(fileContents, match, regex)) {
+        displayErrorAndQuit("Data is not in proper format. Terminating.");
+    }
+
+    return fileContents;
+}
--- src/cs5201-object-oriented-numerical-modeling-ninjas/2018-sp-a-hw2-isgx2/src/functions/functions.h
+++ src/cs5201-object-oriented-numerical-modeling-ninjas/2018-sp-a-hw2-isgx2/src/functions/functions.h
@@ -6,27 +6,154 @@
 //  Copyright 2018. Illya Starikov. All rights reserved.
 //
 
+/** @file */
+
 #ifndef functions_h
 #define functions_h
 
+#include <experimental/filesystem>
 #include <fstream>
 #include <iostream>
-#include <experimental/filesystem>
+#include <regex>
+#include <string>
+#include <type_traits>
+#include <typeinfo>
+#include <vector>
+#include <algorithm>
+
+#include "../lib/polarpair.h"
 
 namespace filesystem = std::experimental::filesystem;
 
-bool enoughParametersPassed(const int argc);
-void displayErrorAndQuit(const std::string output);
+/** @brief Ensures there are enough parameters for the main function.
+ *
+ *  @param argc The number of parameters passed into main (typically named
+ *         @p argc).
+ *
+ *  @warning Because the number of parameters is *critical* for the program
+ *         functioning correctly, if there are not enough parameters, the
+ *         program will terminate prematurely and print out the error to
+ *         `stderr`.
+ *
+ * @pre    @p argc must at least 2.
+ * @post   See `warning` */
+void ensureEnoughArguments(const int argc);
 
-std::vector<std::string> split(const std::string someString, const char delimiter);
-int stringToInt(const std::string someString);
-double stringToDouble(const std::string someString);
+/** @brief Feed an error to `stderr`, and terminate the program.
+ *
+ *  @warning This does terminate the program prematurely, meaning all subsequent
+ *         after this function call will be ignored.
+ *
+ *  @pre   None.
+ *  @post  See `note`. */
+void displayErrorAndQuit(const std::string& output);
 
-std::vector<std::pair<double, double>> parseFileContents(const std::string filename);
-bool fileExists(const std::string filename);
+/** @brief Split the input at @p delimiter (if multiple `delimiter`s) into
+ *  multiple strings.
+ *
+ *  @param someString The spring that is to be split into multiple strings.
+ *  @param delimiter The `char` that @p someString will be split at.
+ *
+ *  @return A `vector` with whose entries are associated with both sides of
+ *  the string after removing @p delimiter and appending the left and right
+ *  hand side of the split.
+ *
+ *  @pre    None.
+ *  @post   See `return`. */
+std::vector<std::string> split(const std::string& someString, const char delimiter);
 
-std::string readFile(filesystem::path path);
+/** @brief Converts a string to an integer.
+ *
+ *  @param someString The string to convert to an `int`. Note that if
+ *  `someString` cannot explicitly be converted to an `int`, i.e., if
+ *  ```
+ *  stringToInt("forty-two"); // invalid
+ *  stringToInt("forty-two-42"); // invalid
+ *  stringToInt(""); // invalid
+ *  stringToInt(".0"); // invalid
+ *  ```
+ *  an exception will be thrown.
+ *
+ *  @return `someString` converted to an `int`.
+ *
+ *  @pre   See @p someString.
+ *  @post  See `return`. */
+int stringToInt(const std::string& someString);
 
+/** @brief Converts a string to an integer.
+ *  @param someString The string to convert to a `float`. Note that if
+ *  `someString` cannot explicitly be converted to an `float`, i.e., if
+ *  ```
+ *  stringToInt("forty-two-dot-two"); // invalid
+ *  stringToInt("forty-two-0.0"); // invalid
+ *  stringToInt(""); // invalid
+ *  stringToInt(".0"); // invalid
+ *  ```
+ *  an exception will be thrown.
+ *  @return `someString` converted to an `float`.
+ *
+ *  @pre   See @p someString.
+ *  @post  See `return`. */
+double stringToDouble(const std::string& someString);
+
+/** @brief Determines if a file exists a specified path.
+ *  @param filename The full or relative path where to check if a file
+ *         exists.
+ *  @return `true` if the file exists, `false` otherwise.
+ *
+ *  @pre   None.
+ *  @post  See `return` */
+bool fileExists(const std::string& filename);
+
+/** @brief Read into a `std::string` from a specified file.
+ *
+ *  @param path The path where file is to be read from. Can
+ *         be relative or absolute.
+ *
+ *  @return An `std::string` with the entirety of the file contents
+ *          in them (`n` and all).
+ *
+ *  @Note This does throw an exception if the file does not exist.
+ *
+ *  @pre  Check `note`.
+ *  @post See `return`.
+ */
+std::string readFile(const filesystem::path& path);
+
+/** @brief Read the specified input file and ensure proper
+ *         data format.
+ *
+ *  @param filename The filename where file is to be read from. Can
+ *         be relative or absolute.
+ *
+ *  @return An `std::string` with the entirety of the file contents
+ *          in them (`n` and all).
+ *
+ *  @Note This does terminate prematurely if the file contents is not
+ *        in the proper format.
+ *
+ *  @pre  Check `note`.
+ *  @post See `return`.
+ */
+std::string readInputFile(const std::string& filename);
+
+/** @brief Parse out Polar Pairs from @p fileContents.
+ *
+ *  @param fileContents The string to parse the Polar Pairs from. This is
+ *         intended to have the `\n` and all.
+ *
+ *  @return A `std::vector` of polar pairs.
+ *
+ *  @tparam R The modulus type of the Polar Pair. See `PolarPair`
+            Documentation for specification of `R`.
+ *  @tparam Theta The argument type of the Polar Pair. See `PolarPair`
+            Documentation for specification of `Theta`.
+**/
+template<typename R, typename Theta>
+std::vector<PolarPair<R, Theta>> parseFileContents(const std::string& fileContents);
+
+
+#include "functions.hpp"
 
 #endif /* functions_h */
 
