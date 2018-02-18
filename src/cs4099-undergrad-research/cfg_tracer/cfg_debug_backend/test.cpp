e6c3075b6e13ec08e2bc85cea8e01878ae10fb36
Nathan Eloe <REDACTED@example.com>
2018-02-18 07:31:46 -0600

    Add test cpp and cfg files

---
+++ src/cs4099-undergrad-research/cfg_tracer/cfg_debug_backend/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+void foo() {int a = 1;}
+int main()
+{
+	int a = 0;
+	double d = 1.1;
+	for (int i=0; cerr<<"bob",i<10; i++)
+		cout << "hi" << endl;
+	return 0;
+}
