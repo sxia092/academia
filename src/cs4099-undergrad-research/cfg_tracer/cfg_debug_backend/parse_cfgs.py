8b65688aaeff019ca2100464644f2dbf8e5dd374
Nathan Eloe <REDACTED@example.com>
2018-03-11 14:52:48 -0500

    Fix reading files with no newline at end

--- src/cs4099-undergrad-research/cfg_tracer/cfg_debug_backend/parse_cfgs.py
+++ src/cs4099-undergrad-research/cfg_tracer/cfg_debug_backend/parse_cfgs.py
@@ -19,7 +19,7 @@ def parse_cfgs(filename):
     cfgs = []
     bblines = []
     with open(filename) as f:
-        #cfgs = parse_lines(f.)
+        #cfgs = parse_lines(f)
         for line in f:
             if line is '\n':
                 cfgs[-1].add_block(bblines)
@@ -28,12 +28,15 @@ def parse_cfgs(filename):
                 cfgs.append(CFG(line))
             else:
                 bblines.append(line.strip())
+        if len(bblines) != 0:
+            cfgs[-1].add_block(bblines)
+            bblines = []
     return cfgs
 
 
 if __name__ == "__main__":
     import os
-    cfg = parse_cfgs('testfiles/if.cfg')
+    cfg = parse_cfgs('out.cfg')
     cfg[0].reverse_labels()
     cfg_json = cfg[0].to_json()
     import json
